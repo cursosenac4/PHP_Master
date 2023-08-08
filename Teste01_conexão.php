c17f5ad6f3aefc6f5887f5639be4b86cff357611
cursosenac4 <131500719+Vitor-Desenvolvedor@users.noreply.github.com>
2023-08-08 20:03:56 -0300

    Add

--- Teste01_conexão.php
+++ Teste01_conexão.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="icon" href="img/Icone_usuário.png">
     <title>Banco de dados - Usuário</title>
 </head>
 <body>
