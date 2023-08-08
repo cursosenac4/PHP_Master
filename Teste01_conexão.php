612b81e21901d75f5ffc7aa0122b76ed93919481
cursosenac4 <131500719+Vitor-Desenvolvedor@users.noreply.github.com>
2023-08-08 19:17:48 -0300

    Update Teste01_conexão.php

--- Teste01_conexão.php
+++ Teste01_conexão.php
@@ -1,3 +1,5 @@
+
+
 <?php 
 
 // Criação de variaveis para conexão com o banco de dados
@@ -6,6 +8,8 @@ $host = 'localhost';
 $usuario = 'root';
 $senha = '';
 $banco = 'db_teste2';
+$codigo = '1';
+$nome = 'Vitor';
 
 // Criação de conexão com o banco de dados
 
@@ -19,5 +23,16 @@ if ($conexao->connect_error){
 } else {
     echo "Parabéns, você conseguiu conectar na base: ".$banco;
 }
+
+//$sql = "SELECT * FROM usuário WHERE Código = 1";
+
+
+
+// Inserir dados na tabela do banco de dados
+
+$sql = "INSERT INTO usuário VALUES ('$codigo','$nome')";
+
+// Executa comando na query
+$resultado = $conexao->query($sql);
 
 ?> 
